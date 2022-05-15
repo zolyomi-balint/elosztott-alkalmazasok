f58aae08ee47c96d3b21907d967386a6ab76bec8
zolyomi-balint <bzolyomi@gmail.com>
2022-05-15 21:12:45 +0200

    added NameComparator class

---
+++ src/beadando/NameComparator.java
@@ -0,0 +1,12 @@
+package beadando;
+
+import java.util.Comparator;
+
+public class NameComparator  implements Comparator<Person> {
+
+    @Override
+    public int compare(Person p1, Person p2) {
+        return p1.getName().compareToIgnoreCase(p2.getName());
+    }
+    
+}
