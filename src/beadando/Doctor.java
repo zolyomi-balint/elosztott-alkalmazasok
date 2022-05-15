e19ab4947a0fe080e529b9bc953615e0083c3c5c
zolyomi-balint <bzolyomi@gmail.com>
2022-05-15 20:32:34 +0200

    added Doctor class

---
+++ src/beadando/Doctor.java
@@ -0,0 +1,20 @@
+package beadando;
+
+public class Doctor extends Person {
+    private String specialization;
+
+    public Doctor(String name, int age, String specialization) {
+        super(name, age);
+        this.specialization = specialization;
+    }
+
+    @Override
+    public void chat(Person person) {
+        System.out.println(this.getName() + " chatting with: " + person.getName());
+    }
+
+    public String getSpecialization() {
+        return specialization;
+    }
+    
+}
