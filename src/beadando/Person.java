7ba37681011e0849eb4ad76887b8069216cbb584
zolyomi-balint <bzolyomi@gmail.com>
2022-05-15 20:26:10 +0200

    added methods to Person class

--- src/beadando/Person.java
+++ src/beadando/Person.java
@@ -4,4 +4,33 @@ public class Person {
     private String name;
     private int age;
     private boolean isHealthy;
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+        this.isHealthy = true;
+    }
+    
+    public void chat(Person person) {
+        System.out.println(this.name + " chatting with: " + person.name);
+        this.setIsHealthy(Math.random() < .9);
+        person.setIsHealthy(Math.random() < .9);
+    }
+
+    public void setIsHealthy(boolean isHealthy) {
+        this.isHealthy = isHealthy;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public boolean isIsHealthy() {
+        return isHealthy;
+    }
+    
 }
